b49c91cc7183e63b733961c500c83ba7c115c186
i-am-sai <saiswaroopkdevarmane@gmail.com>
2023-08-14 01:02:29 +0530

    File Structure

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+
 app.post("/", function (req, res) {
 
     const query = req.body.cityName;
@@ -25,75 +26,45 @@ app.post("/", function (req, res) {
     https.get(url, function (response) {
         console.log(response, STATUS_CODES);
 
-        response.on("data", function (data) {
-            const weatherData = JSON.parse(data) // Converts Hexadecimal data to string format.
-            const temp = weatherData.main.temp                // JS object
-            const temp1 = weatherData.weather[0].description
+        response.on("data", function (responseData) {
+            const weatherData = JSON.parse(responseData);
+            const temp = weatherData.main.temp;
+            const temp1 = weatherData.weather[0].description;
+            const pressure = weatherData.main.pressure;
+            const humidity = weatherData.main.humidity;
+            const speed = weatherData.wind.speed;
+
             console.log(temp);
             console.log(weatherData);
             console.log(temp1);
 
-            const icon = weatherData.weather[0].icon
+            const icon = weatherData.weather[0].icon;
             const imageURL = " https://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-            res.write("<!DOCTYPE html>");
-            res.write("<html>");
-            res.write("<head>");
-            res.write("<title>Weather Report</title>");
-            res.write("<style>");
-            res.write("body {");
-            res.write("background-color: #F0F8FF;");
-            res.write("font-family: Arial, sans-serif;");
-            res.write("}");
-            res.write("h1 {");
-            res.write("font-size: 2em;");
-            res.write("text-align: center;");
-            res.write("}");
-            res.write(".weather-info {");
-            res.write("display: flex;");
-            res.write("justify-content: center;");
-            res.write("align-items: center;");
-            res.write("}");
-            res.write(".weather-icon {");
-            res.write("margin-right: 1em;");
-            res.write("}");
-            res.write("</style>");
-            res.write("</head>");
-            res.write("<body>");
-            res.write("<h1>Current Weather in " + query + "</h1>");
-            res.write("<div class='weather-info'>");
-            res.write("<div class='weather-icon'>");
-            res.write("<img src='" + imageURL + "' alt='weather icon'>");
-            res.write("</div>");
-            res.write("<div class='weather-description'>");
-            res.write("<p>The temperature is <strong>" + temp + " &#8451;</strong></p>");
-            res.write("<p>The weather is currently <strong>" + temp1 + "</strong></p>");
-            res.write("</div>");
-            res.write("</div>");
-            res.write("</body>");
-            res.write("</html>");
-            res.send();
+            const data = {
+                city: query,
+                imageURL: imageURL,
+                temp: temp,
+                temp1: temp1,
+                pressure: pressure,
+                humidity: humidity,
+                speed: speed
+            };
+
+            res.render("weather.ejs", data);
+        });
+    });
+});
+
+app.post("/", function (req, res) {
+
+});
 
 
-        })
-    })
-})
 
-<<<<<<< HEAD
 app.listen(8080, function () {
 
     console.log("Server is running on port 8080.");
-=======
-app.listen(3000, function){
 
-    console.log("Server is running on port 3000";
->>>>>>> f67293c30e9b01c5a2b6b7ab9354305afaae22a0
-})
+});
 
-
-/* 
-res.write("<h1>The temperature in " + query + " is " +  temp + " celsius</h1>");
-        res.write("<p> The weather is currently " + temp1 +" clouds");
-        res.write("<img src="  + imageURL +  "></img>");
-        res.send();
-*/
